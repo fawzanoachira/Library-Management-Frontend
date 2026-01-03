ede97a26c1e45747d88dcd6e99fde3df5793e72a
fawzanoachira <fawzanoachira@gmail.com>
2026-01-03 18:07:48 +0530

    ui changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: "users", loadChildren: () => import("./users/users.module").then(m => m.UsersModule) },
+  { path: 'books', loadChildren: () => import('./books/books.module').then(m => m.BooksModule) },
   { path: "", redirectTo: "users/get", pathMatch: "full" },
   { path: "**", redirectTo: "users/get" }
 ];
